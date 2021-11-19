6c9d0d93652e6cd632eb367857d6c01af73641f0
najmus2808 <najmus2808@gmail.com>
2021-11-19 22:29:35 +0600

    example of context api is done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ClickCounter from "./components/ClickCounter";
+import Counter from "./components/Counter";
+import Section from "./components/Section";
+import ThemeContext from "./contexts/themeContext";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  state = {
+    // eslint-disable-next-line react/no-unused-state
+    theme: "light",
+    // eslint-disable-next-line react/no-unused-state
+    switchTheme: () => {
+      this.setState(({ theme }) => {
+        if (theme === "dark") {
+          return {
+            theme: "light",
+          };
+        }
+        return {
+          theme: "dark",
+        };
+      });
+    },
+  };
+
+  render() {
+    return (
+      <div className="app">
+        <Counter>
+          {(counter, incrementCount) => (
+            <ClickCounter count={counter} incrementCount={incrementCount} />
+          )}
+        </Counter>
+        <ThemeContext.Provider value={this.state}>
+          <Section />
+        </ThemeContext.Provider>
+      </div>
+    );
+  }
 }
-
-export default App;
